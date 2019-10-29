962b14d36152ce1ebb20c27763b97dfc21b4404d
TheAct1on <luparev22@list.ru>
2019-10-29 23:41:57 +0700

    Create simple window

--- source/code/Battle City.cpp
+++ source/code/Battle City.cpp
@@ -2,20 +2,30 @@
 //
 
 #include "pch.h"
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
-int main()
-{
-    std::cout << "Hello World!\n"; 
+using namespace sf;
+
+int main() {
+	srand(time(NULL));
+	int wx = 1280, wy = 720;
+	sf::Clock clock;
+
+	RenderWindow window(VideoMode(wx, wy), "Battle City");
+
+	while (window.isOpen()) {
+		float dt = clock.restart().asSeconds();
+		Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == Event::Closed || event.key.code == Keyboard::Escape) {
+				window.close();
+			}
+		}
+
+
+		window.clear();
+		window.display();
+	}
+
+	return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
