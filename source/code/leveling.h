286df18d3e9f5ecad6dfbfcd5e05c984cf5677da
Игорь Утешев <dragonmount176@gmail.com>
2019-11-01 20:41:06 +0700

    Add map reader and draw map

---
+++ source/code/leveling.h
@@ -0,0 +1,48 @@
+#pragma once
+#ifndef LEVEL_H
+#define LEVEL_H
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <vector> 
+using namespace sf;
+
+class Tile;
+
+class LevelManager {
+private:
+	char currentMap[13][13];
+	Image image;
+public:
+	std::vector<Tile*> tiles;
+	LevelManager(Image img) {
+		image = img;
+	}
+	void ReadMap(int n);
+	void DrawMap();
+	
+};
+
+class Tile {
+private:
+	Texture texture;
+	Sprite sprite;
+	int x;
+	int y;
+	bool isBreakable;
+	bool isDriveable;
+	int layout;
+public:
+	int getLayout() { return layout; };
+	Sprite* getSprite() { return &sprite; };
+	Tile(Image img, int x, int y, int x_image, int y_image, int W, int H,bool breakable,bool drive,int layout)
+	: x(x), y(y),isBreakable(breakable),isDriveable(drive),layout(layout)
+	{
+		texture.loadFromImage(img);
+		sprite.setTexture(texture);
+		sprite.setTextureRect(IntRect(x_image, y_image, W, H));
+		sprite.setPosition(x, y);
+		sprite.setScale(3, 3);
+	}
+};
+
+#endif
